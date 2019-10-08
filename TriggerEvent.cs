cc6b80b7dd579d1f928aadd9cb88d09f6684af82
StijnOnline <stijnvandeijzen@hotmail.nl>
2019-10-08 17:10:44 +0200

    Script for triggering an UnityEvent when something enters the trigger collider

---
+++ TriggerEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyTriggerEvent : UnityEvent<Collider> { }
+
+public class TriggerEvent : MonoBehaviour
+{
+    public MyTriggerEvent triggerEvent = new MyTriggerEvent();
+
+    private void OnTriggerEnter(Collider other) {
+        triggerEvent.Invoke(other);
+    }
+}
