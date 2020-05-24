35326239fa15a917df03a6f6c9411f8c1b2f18dc
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2020-05-24 14:21:25 +0200

    Added WIP Hierarchy Fix Tool

---
+++ StijnUtilityScripts/Editor/HierarchyFixerToolWindow.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HierarchyFixerToolWindow : EditorWindow {
+    [MenuItem("Window/Hierarchy Fixer")]
+    static void Init() {
+        HierarchyFixerToolWindow window = (HierarchyFixerToolWindow)EditorWindow.GetWindow(typeof(HierarchyFixerToolWindow));
+        //Debug.Log(StringCompare.findstem(new string[] {"test Boom","Boom 3 (Clone)","boomding"}));
+        //Debug.Log(StringCompare.SimpleComparison("Boom", "Boom (3) (Clone)"));
+    }
+
+    void OnGUI() {
+        GUILayout.Label("Testing");
+        if(GUILayout.Button("Scan")) {
+            List<GameObject> ObjectsToScan = new List<GameObject>();
+            SceneManager.GetActiveScene().GetRootGameObjects(ObjectsToScan);
+
+
+            ObjectGroup[] foundgroups = Scan(ObjectsToScan);
+
+
+            foreach(var foundGroups in foundgroups) {
+                Debug.Log("Group " + foundGroups.commonName);
+                foreach(var groupObjects in foundGroups.objects) {
+                    Debug.Log(groupObjects.name);
+                }
+            }
+        }
+    }
+
+    private ObjectGroup[] Scan(List<GameObject> _objectsToScan) {
+
+        List<GameObject> unsorted = new List<GameObject>();
+        List<ObjectGroup> groups = new List<ObjectGroup>();
+
+        while(unsorted.Count > 0) {
+            bool Done = false;
+            GameObject currentScanning = unsorted[0];
+            unsorted.RemoveAt(0);
+
+            foreach(var group in groups) {
+                if(StringCompare.SimpleComparison(group.commonName, currentScanning.name)) {
+                    group.objects.Add(currentScanning);
+                    Done = true;
+                    break;
+                }
+            }
+            if(!Done) {
+                foreach(var unsortedObject in unsorted) {
+                    if(StringCompare.SimpleComparison(unsortedObject.name, currentScanning.name)) {
+                        ObjectGroup newGroup = new ObjectGroup(StringCompare.ReduceName(currentScanning.name));
+                        newGroup.objects.Add(currentScanning);
+                        newGroup.objects.Add(unsortedObject);
+                        unsorted.Remove(unsortedObject);
+                        groups.Add(newGroup);
+                        Done = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return groups.ToArray();
+    }
+
+    class ObjectGroup {
+        public string commonName { get; private set; }
+        public List<GameObject> objects = new List<GameObject>();
+
+        public ObjectGroup(string _commonName) {
+            commonName = _commonName;
+        }
+    }
+}
---
+++ StijnUtilityScripts/StringCompare.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class StringCompare{
+
+    public const string MATCH_CLONE_TEXT = @"(\s*\([cC]lone\))";
+    public const string MATCH_NUMBER_TEXT = @"(\s*\(+\d+\)+)|(\s*\d+\s)|(\s*_\d+)";
+
+    public static bool SimpleComparison(string a, string b) {        
+        return ReduceName(a) == ReduceName(b);
+    }
+
+	public static string FindStem(String[] arr, bool CaseSensitive = false) {
+        int n = arr.Length;
+
+        String s = arr[0].ToLower();
+        int len = s.Length;
+
+        String res = "";
+
+        for(int i = 0; i < len; i++) {
+            for(int j = i + 1; j <= len; j++) {
+
+                String stem = s.Substring(i, j - i);
+                int k = 1;
+                for(k = 1; k < n; k++) {
+                    if(!arr[k].ToLower().Contains(stem))
+                        break;
+                }
+                if(k == n && res.Length < stem.Length)
+                    res = stem;
+            }
+        }
+
+        return res;
+    }
+
+    public static string[] Group(String[] arr, bool CaseSensitive = false) {
+        return null;
+    }
+    private static string ReplaceStuff(string input,string[] Matches) {        
+        return Regex.Replace(input, string.Join("|",Matches), "");
+    }
+
+    public static string ReduceName(string input) {
+        string[] match = { MATCH_CLONE_TEXT, MATCH_NUMBER_TEXT };
+        return ReplaceStuff(input, match);
+    }
+}
