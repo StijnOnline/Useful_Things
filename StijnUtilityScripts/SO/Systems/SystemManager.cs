4eae0a01d4e37a534652b88217fa850ae4d8e760
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2021-06-21 21:06:23 +0200

    Systems

---
+++ StijnUtilityScripts/SO/Systems/SystemManager.cs
@@ -0,0 +1,24 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour {
+
+    [ReorderableList] public List<GameSystem> systems;
+
+    void Start()
+    {
+
+        foreach ( GameSystem system in systems ) {
+            system.Init();
+        }
+    }
+
+    void Update()
+    {
+        foreach ( GameSystem system in systems ) {
+            system.Update();
+        }
+    }
+}
