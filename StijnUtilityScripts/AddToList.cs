d57e99cdddeaee90c4a310b87a2dc90bab00278f
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2021-06-21 21:16:07 +0200

    add to list, clear on load for lists

---
+++ StijnUtilityScripts/AddToList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToList : MonoBehaviour
+{
+    public GameObjectSOList list;
+    public enum Mode {
+        Enable,
+        EnableDisable,
+        Awake,
+        AwakeDestroy,
+    }
+    public Mode mode;
+
+    private void OnEnable() {
+        if(mode== Mode.EnableDisable || mode == Mode.Enable)
+            list.List.Add(gameObject);
+    }
+
+    private void OnDisable() {
+        if ( mode == Mode.EnableDisable )
+            list.List.Remove(gameObject);
+    }
+
+    private void Awake() {
+        if ( mode == Mode.AwakeDestroy || mode == Mode.Awake )
+            list.List.Add(gameObject);
+    }
+
+    private void OnDestroy() {
+        if ( mode == Mode.AwakeDestroy )
+            list.List.Remove(gameObject);
+    }
+}
--- StijnUtilityScripts/SO/Lists/SOList.cs
+++ StijnUtilityScripts/SO/Lists/SOList.cs
@@ -3,5 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class SOList<T> : ScriptableObject {
+    public bool clearOnLoad;
     [NaughtyAttributes.ReorderableList] public List<T> List = new List<T>();
+
+    private void OnEnable() {
+        if ( clearOnLoad ) List.Clear();
+    }
 }
