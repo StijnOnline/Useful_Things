1edef9011de1a803679af64766f2df83cf67a348
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2021-06-23 21:57:58 +0200

    no message

---
+++ StijnUtilityScripts/SO/Actions/DebugAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DebugAction", menuName = "ScriptableObjects/Action/DebugAction")]
+public class DebugAction : ScriptableObject
+{
+    public void Log(string message ) {
+        Debug.Log(message);
+    }
+
+    public void Log(GameObject message ) {
+        Debug.Log(message);
+    }
+}
