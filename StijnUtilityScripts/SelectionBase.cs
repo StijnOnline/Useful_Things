0d0b04be76bf9ecec187b2280890154648171004
Stijn van Deijzen <stijnvandeijzen@hotmail.nl>
2021-06-23 21:58:08 +0200

    no message

---
+++ StijnUtilityScripts/SelectionBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StijnUtility
+{
+    [SelectionBase]
+    public class SelectionBase : MonoBehaviour {}
+}
